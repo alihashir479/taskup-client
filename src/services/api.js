bcff48df7768b629ed6dbdaebb2f963553d1625f
Hashir <alihashir479@gmail.com>
2023-01-19 11:51:59 +0500

    add cors

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const axiosInstance = axios.create({
-    baseURL: "https://taskup-server.vercel.app/",
+    baseURL: "https://taskup-server-alihashir479.vercel.app/",
     withCredentials: true
 })
 
