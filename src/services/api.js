bb7e9595d91ef50d3a448b4429108f43436edf2d
Hashir <alihashir479@gmail.com>
2023-01-19 14:23:44 +0500

    change cors

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const axiosInstance = axios.create({
-    baseURL: "https://taskup-server-alihashir479.vercel.app/api/",
+    baseURL: "https://taskup-server.vercel.app/api/",
     withCredentials: true
 })
 
